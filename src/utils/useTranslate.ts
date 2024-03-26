b6aa5890ef1f8e85df9c89b625c2c28de712c829
YYGod <2569630663@qq.com>
2024-03-26 17:24:14 +0800

    fix: 导出bug以及中文类别

--- src/utils/useTranslate.ts
+++ src/utils/useTranslate.ts
@@ -1,5 +1,6 @@
 import { saveAs } from 'file-saver';
 import { asBlob } from 'html-docx-js-typescript';
+import { marked } from 'marked';
 
 import { useConversationStore } from '@/store';
 type HtmlString = Record<'content' | 'title', string>[];
@@ -13,7 +14,7 @@ function HtmlContent(strs: HtmlString) {
   <body>
     ${strs.map((str) => {
       return `<h4>${str.title}</h4>
-      <p>${str.content}<p/>
+      <div>${str.content}<div/>
       <hr></hr>
       `;
     })}
@@ -23,17 +24,38 @@ function HtmlContent(strs: HtmlString) {
 export function useTranslateHtml() {
   const conversations = useConversationStore((state) => state.conversation);
   const conversation_title = useConversationStore((state) => state.title);
+  const dealAIres_conversations = async () => {
+    const processedConversations = await Promise.all(
+      conversations.map(async (conversation) => {
+        if (conversation.AI) {
+          const ai_res = await marked.parse(conversation.AI[0].answer);
+          return {
+            AI: ai_res,
+            time: conversation.time,
+            HUMAN: conversation.HUMAN,
+            message_id: conversation.message_id,
+          };
+        }
+        return conversation;
+      })
+    );
+    return processedConversations;
+  };
   return async function (id?: number) {
     if (id) {
       if (id > 0) {
-        const conversation_ai = conversations[id];
-        const conversation_user = conversations[id - 1];
+        const conversation = await dealAIres_conversations();
+
+        const conversation_ai = conversation[id];
+        const conversation_user = conversation[id - 1];
+        // //对ai回答进行处理
+        // const aiRes = await marked.parse(conversation_ai.AI[0].answer);
         const content = [
           {
             content: conversation_user.HUMAN,
             title: 'HUMAN' + ' ' + conversation_user.time,
           },
-          { content: conversation_ai.AI, title: 'AI' },
+          { content: conversation_ai.AI as string, title: 'AI' },
         ];
         const contents = HtmlContent(content);
         const rep = await asBlob(contents);
@@ -41,7 +63,7 @@ export function useTranslateHtml() {
       } else {
         const content = [
           {
-            content: conversations[id].AI,
+            content: conversations[0].AI[0].answer,
             title: 'AI',
           },
         ];
@@ -50,11 +72,13 @@ export function useTranslateHtml() {
         saveAs(rep, `${conversation_title}.doc`);
       }
     } else {
-      const strs: HtmlString = conversations.map((conversation) => {
+      const conversation = await dealAIres_conversations();
+      const strs: HtmlString = conversation.map((conversation) => {
         const conversation_identity = Object.keys(conversation)[0];
         const conversation_time = Object.values(conversation)[1]
           ? Object.values(conversation)[1]
           : '';
+
         return {
           content: conversation[conversation_identity],
           title: conversation_identity + ' ' + conversation_time,
